323871a78f3a121936c090d325d726ca59542bea
Kevin Mack <kmack@manicmachinegames.com>
2018-10-25 17:50:32 -0700

    Initial commit

---
+++ Source/BasicCodeTest/BasicCodeTestGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasicCodeTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICCODETEST_API ABasicCodeTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
